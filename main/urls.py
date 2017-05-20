977362d8a3e43c797ec8dc348f7b8cb636e1af18
Marcin Rakowski <marcin@rakowscy.pl>
2017-05-20 12:20:22 +0000

    add comments

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from main import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    url(r'^$', views.home, name='home_page'),
-   url(r'article/(?P<post_id>\d+)/$', views.post_show, name='post_show')
+   url(r'article/(?P<post_id>\d+)/$', views.post_show, name='post_show'),
+   url(r'^login/$', auth_views.login, {'template_name':'user/login.html'}, name='login'),
+   url(r'^logout/$', auth_views.logout, {}, name='logout')
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import render, redirect
 from models import Article
+from comment.forms import CommentForm
 
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -18,6 +20,20 @@ def post_show(request, post_id):
     if not article:
         raise ObjectDoesNotExist
         
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.commentator = request.user.commentator
+            comment.created_date = timezone.now()
+            comment.save()
+            redirect('post_show', {'post_id':article.id})
+    else:
+        comment_form = CommentForm(initial={
+            'article': post_id
+        })
+    
     return render(request, 'post_show.html', {
-        'article':article
+        'article':article,
+        'comment_form':comment_form
     })
